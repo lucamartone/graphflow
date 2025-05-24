bea406a22d24f8feb972b36d3ad6d116f28c958d
lucamartone <luca.martone14@gmail.com>
2025-05-24 20:31:39 +0200

    Delete GraphFlow-main 2 directory

--- GraphFlow-main 2/php/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-//permette di vedere la sezione saved graph solo se sei loggato
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../index.html");
-    exit();
-}
-?> 
